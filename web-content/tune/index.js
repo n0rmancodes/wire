cba8bf86de6cbaf732cf18e290a47c876eb59a1f
norman <41760554+n0rmancodes@users.noreply.github.com>
2020-08-24 12:09:36 -0400

    bug fixes

--- web-content/tune/index.js
+++ web-content/tune/index.js
@@ -7,19 +7,25 @@ function load() {
     xhr.send();
     xhr.onload = function () {
         var json = JSON.parse(xhr.responseText);
-        document.getElementById("load").style.display = "none";
-        document.getElementById("profile").style.display = "";
-        document.getElementById("cover").src = "/proxy/" + btoa(json.musicInfo.coverMedium);
-        document.getElementById("title").innerHTML = json.musicInfo.musicName;
-        document.title = json.musicInfo.musicName + " | WireTick";
-        document.getElementById("authorName").innerHTML = json.musicInfo.musicAuthor;
-        for (var c in json.music.collector) {
-            var link = document.createElement("A");
-            link.href = json.music.collector[c].webVideoUrl.split("https://www.tiktok.com")[1];
-            var img = document.createElement("IMG");
-            img.src = "/proxy/" + btoa(json.music.collector[c].covers.origin);
-            link.appendChild(img);
-            document.getElementById("feed").appendChild(link);
+        if (!json.err) {
+            document.getElementById("load").style.display = "none";
+            document.getElementById("profile").style.display = "";
+            document.getElementById("cover").src = "/proxy/" + btoa(json.musicInfo.coverMedium);
+            document.getElementById("title").innerHTML = json.musicInfo.musicName;
+            document.title = json.musicInfo.musicName + " | WireTick";
+            document.getElementById("authorName").innerHTML = json.musicInfo.musicAuthor;
+            for (var c in json.music.collector) {
+                var link = document.createElement("A");
+                link.href = json.music.collector[c].webVideoUrl.split("https://www.tiktok.com")[1];
+                var img = document.createElement("IMG");
+                img.src = "/proxy/" + btoa(json.music.collector[c].covers.origin);
+                link.appendChild(img);
+                document.getElementById("feed").appendChild(link);
+            }
+        } else {
+            document.getElementById("err").style.display = "";
+            document.getElementById("load").style.display = "none";
+            document.getElementById("errTxt").innerHTML = json.err;
         }
     }
     
